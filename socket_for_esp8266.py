c34db6bdf343302f73b975f3c44855e5428b9bb6
mitradeniz <65504062+mitradeniz@users.noreply.github.com>
2023-07-11 00:47:43 +0300

    Update socket_for_esp8266.py
    Socket TCP protokolu üzerinden localhost/password/hareketbilgisi apisi ile hareket olup olmadığını görebiliyoruz

--- socket_for_esp8266.py
+++ socket_for_esp8266.py
@@ -1,9 +1,10 @@
 import socket
-
+from flask import Flask
+import time
 
 host = '0.0.0.0'  # Tüm ip adreslerinden veri kabul eder
 port = 8080
-
+data = '0'
 # Soket oluştur
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
@@ -11,21 +12,24 @@ s.listen(1)
 
 print(f"Port {port} dinleniyor...")
 
-while True:
-    conn, addr = s.accept()
-    print('Bağlantı:', addr)
-    data = conn.recv(1024).decode('utf-8') # recv(1024) alacağı maksimum byte sayısını gösterir decode(utf-8) ise hangi türde karakter çözümlemesi yapacağını gösterir
-    print('Gelen veri:', data)
-    
-    if data == '1':
-        response = 'Hareket algılandı!'
-    elif data == '0':
-        response = 'Hareket yok!'
-    else:
-        response = 'Geçersiz veri!'
-    
-    # Sunucuya response yolla
-    conn.sendall(response.encode('utf-8'))
-    
-    conn.close()
+app = Flask(__name__)
 
+hareket_bilgisi = "Hareket bilgisi yok."
+
+@app.route('/password/hareketbilgisi', methods=['GET'])
+def hareketBilgisi():
+	conn, addr = s.accept()
+	
+	data = conn.recv(1024).decode('utf-8')
+	conn.close()
+
+	if data == "1":
+		return "Hareket Algılandı!!" + data
+	elif data == "0":
+		return "Hareket Algılanmadı" + data
+	else:
+		return "Hareket bilgisi yok" + str(data)
+
+
+if __name__ == '__main__':
+    app.run(port=8081)	
